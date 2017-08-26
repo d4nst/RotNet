b41ed03e4882ea34f0796e0bb189414c2c42e322
d4nst <danst.18@gmail.com>
2017-08-26 16:08:19 +0100

    Update to Keras2 api

--- train/train_mnist.py
+++ train/train_mnist.py
@@ -6,12 +6,10 @@ import numpy as np
 from keras.callbacks import ModelCheckpoint, EarlyStopping, TensorBoard
 from keras.datasets import mnist
 from keras.layers import Dense, Dropout, Flatten, Input
-from keras.layers import Convolution2D, MaxPooling2D
+from keras.layers import Conv2D, MaxPooling2D
 from keras.models import Model
 
 from utils import angle_error, RotNetDataGenerator, binarize_images
-
-
 # we don't need the labels indicating the digit value, so we only load the images
 (X_train, _), (X_test, _) = mnist.load_data()
 
@@ -19,7 +17,7 @@ from utils import angle_error, RotNetDataGenerator, binarize_images
 X_train = np.expand_dims(X_train, axis=3).astype('float32')
 X_test = np.expand_dims(X_test, axis=3).astype('float32')
 
-model_name = 'rotnet_mnist'
+model_name = 'rotnet_mnist_test'
 
 # number of convolutional filters to use
 nb_filters = 64
@@ -40,10 +38,8 @@ print(nb_test_samples, 'test samples')
 
 # model definition
 input = Input(shape=(img_rows, img_cols, img_channels))
-x = Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
-                  activation='relu')(input)
-x = Convolution2D(nb_filters, kernel_size[0], kernel_size[1],
-                  activation='relu')(x)
+x = Conv2D(nb_filters, kernel_size, activation='relu')(input)
+x = Conv2D(nb_filters, kernel_size, activation='relu')(x)
 x = MaxPooling2D(pool_size=(2, 2))(x)
 x = Dropout(0.25)(x)
 x = Flatten()(x)
@@ -51,7 +47,7 @@ x = Dense(128, activation='relu')(x)
 x = Dropout(0.25)(x)
 x = Dense(nb_classes, activation='softmax')(x)
 
-model = Model(input=input, output=x)
+model = Model(inputs=input, outputs=x)
 
 model.summary()
 
@@ -84,14 +80,14 @@ model.fit_generator(
         preprocess_func=binarize_images,
         shuffle=True
     ),
-    samples_per_epoch=nb_train_samples,
-    nb_epoch=nb_epoch,
+    steps_per_epoch=nb_train_samples / batch_size,
+    epochs=nb_epoch,
     validation_data=RotNetDataGenerator(
         X_test,
         batch_size=batch_size,
         preprocess_func=binarize_images
     ),
-    nb_val_samples=nb_test_samples,
+    validation_steps=nb_test_samples / batch_size,
     verbose=1,
     callbacks=[checkpointer, early_stopping, tensorboard]
 )
