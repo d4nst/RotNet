8c54c3341429b70f7d85f7b9c0384fb989423ffa
Patrick Brueckner <pb@fino.digital>
2019-10-02 15:05:08 +0200

    wget will escape the url, prevent double escaping

--- data/street_view.py
+++ data/street_view.py
@@ -8,7 +8,7 @@ import zipfile
 def download(output_dir):
     for i in range(10):
         filename = 'part{}.zip'.format(i + 1)
-        url = r'http://www.cs.ucf.edu/~aroshan/index_files/Dataset_PitOrlManh/zipped%20images/' + filename
+        url = r'http://www.cs.ucf.edu/~aroshan/index_files/Dataset_PitOrlManh/zipped images/' + filename
         print('Downloading', url)
         filepath = wget.download(url, out=os.path.join(output_dir))
 
